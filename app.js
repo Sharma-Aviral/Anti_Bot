93f248a88f5aa20b3bea3483ce31887935e08fd3
Guest User <Guest@Avis-MacBook-Air.local>
2021-04-09 11:15:21 +0530

    first desu

---
+++ app.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+client.on("ready", () => {
+  console.log("BOT ready");
+});
+
+client.on("message", (msg) => {
+  if (msg.content ===`${config.prefix}test`) {
+    msg.reply("working"+`${msg.author.username}id:${msg.author.id}`);
+  }
+});
+
+
+client.login(config.tokken);
